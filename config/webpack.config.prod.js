9501a92c573bd9afadaa58d80c721f862e63c234
Dustin W <dweaver@appirio.com>
2018-06-22 08:24:15 -0500

    Update webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,11 +12,10 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-
-{
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+  {
   plugins: [ new UglifyJSPlugin() ]
-}
+  };
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
